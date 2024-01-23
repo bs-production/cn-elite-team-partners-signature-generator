ff0eb7ac3deec1e246e5e604069daaf92adb86be
chrisroselli <croselli@contractornation.com>
2024-01-23 16:35:11 -0500

    Update links

--- src/components/output-signature.vue
+++ src/components/output-signature.vue
@@ -63,7 +63,7 @@
                   "vertical-align: top; color: #333333; font-size: 14px; font-family: Arial, Helvetica, sans-serif;">
                   <a href="https://www.cnairgroup.com" target="_blank" style=
                   "color: #333333; text-decoration: none; font-weight: normal; font-size: 14px;">
-                    cnairgroup.com/</a>
+                    cnairgroup.com</a>
                     &nbsp;&nbsp;
                   </td>
                 </tr>
